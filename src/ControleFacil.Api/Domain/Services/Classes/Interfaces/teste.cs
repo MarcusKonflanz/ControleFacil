40532cca76116be0b6ec6fde300e2fe48eb2e302
Marcus Konflanz <vi.i.konflanz@gmail.com>
2024-10-28 21:13:01 -0300

    criacao contrato usuario

---
+++ src/ControleFacil.Api/Domain/Services/Classes/Interfaces/teste.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFacil.Api.Domain.Services.Classes.Interfaces
+{
+    public class teste
+    {
+        
+    }
+}
